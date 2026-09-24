1d960f3667b425cb4d129e8ac4a621cf463337c5
Emm-cd <emmarj1909mail.com>
2026-09-23 18:45:28 -0600

    ft-ERJ: Configuracion de CORS en los servicios para la comunicacion con el frontend

--- Admin/Admin.Api/Program.cs
+++ Admin/Admin.Api/Program.cs
@@ -6,6 +6,22 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// -- EMMA 23/09/2026: SE AGREGO LO DE CORS PARA QUE EL FRONT PUEDA CONSUMIR EL API
+// CONFIG DE CORS
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("FrontendPolicy", policy =>
+    {
+        policy.WithOrigins(
+                "http://localhost:8100",             // ESTE ES DESARROLLO LOCAL
+                "https://falta-dominio-frontend.com"    // AJUSTAMOS ESTE PA CUANDO YA ESTE ARRIBA 
+              )
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+})
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,7 +30,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+app.UseCors("FrontendPolicy");
 
 app.UseAuthorization();
 
